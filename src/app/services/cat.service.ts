4b344fcc1389bc6d8eecad04c4fad4e2e6b13abc
Mariana Pereira <marianappereira219@gmail.com>
2022-08-10 09:03:39 -0300

    button fav

--- src/app/services/cat.service.ts
+++ src/app/services/cat.service.ts
@@ -1,59 +1,57 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map, Observable, retry } from 'rxjs';
-
+ 
 import { AllCats } from '../models/allcats.model';
-
+ 
 @Injectable({
   providedIn: 'root'
 })
-
+ 
 export class CatService {
   api: string;
   apiKey: string;
   user: string;
   header: HttpHeaders;
-
-  
-
+ 
   constructor(private http: HttpClient) {
     this.api = "";
     this.apiKey = "feba2818-3e79-4325-8013-db6f46e90df4";
     this.user = "Mariana";
-
+ 
     this.header = new HttpHeaders({
       'x-api-key': this.apiKey
     })
   }
-
+ 
   // HOME
   getRacas(): Observable<AllCats> {
-    this.api = `https://api.thecatapi.com/v1/images/search?has_breeds=1&limit=16&api_key=${this.apiKey}`;
-
-    return this.http.get<AllCats>(this.api);
+    this.api = `https://api.thecatapi.com/v1/images/search?has_breeds=1&limit=16`;
+ 
+    return this.http.get<AllCats>(this.api, {headers: this.header});
   }
   // DETAILS
   detailsId(id: string): Observable<any>{
-    this.api = `https://api.thecatapi.com/v1/images/${id}?api_key=${this.apiKey}`;
-
-    return this.http.get<any>(this.api);
+    this.api = `https://api.thecatapi.com/v1/images/${id}`;
+ 
+    return this.http.get<any>(this.api, {headers: this.header});
   }
   // FAVOURITES
   postFavs(id: string): Observable<any>{
-    this.api = `https://api.thecatapi.com/v1/favourites?api_key=${this.apiKey}`;
-
-    return this.http.post<any>(this.api, {image_id: id, sub_id: 'Mariana'})
+    this.api = `https://api.thecatapi.com/v1/favourites`;
+ 
+    return this.http.post<any>(this.api, {image_id: id, sub_id: 'Mariana'}, {headers: this.header})
   }
-
+ 
   allFavs(): Observable<any>{
-    this.api = `https://api.thecatapi.com/v1/favourites?sub_id=${this.user}&api_key=${this.apiKey}`;
-
-    return this.http.get<any>(this.api);
+    this.api = `https://api.thecatapi.com/v1/favourites?sub_id=${this.user}`;
+ 
+    return this.http.get<any>(this.api, {headers: this.header});
   }
-
+ 
   deleteFav(idFav: string): Observable<any> {
     this.api = `https://api.thecatapi.com/v1/favourites/${idFav}`;
-
+ 
     return this.http.delete<any>(this.api, {headers: this.header});
   }
 }
